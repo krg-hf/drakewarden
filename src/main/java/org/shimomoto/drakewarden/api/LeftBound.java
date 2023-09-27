17551694c6c06d6aa04fe486bb13955ccf9ce843
krg-hf <135822089+krg-hf@users.noreply.github.com>
2023-09-27 16:52:24 +0530

    Restored LeftBound.java

--- src/main/java/org/shimomoto/drakewarden/api/LeftBound.java
+++ src/main/java/org/shimomoto/drakewarden/api/LeftBound.java
@@ -1,7 +1,6 @@
 package org.shimomoto.drakewarden.api;
 
 public interface LeftBound<T extends Comparable<T>> extends Range<T> {
-	// comment
 	T getLeft();
 	boolean isLeftClosed();
 	default boolean isLeftOpen(){return !isLeftClosed();}
